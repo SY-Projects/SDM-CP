f2641569fd3a3d8471aa7292f0bb23d4bb845efe
Devang Manjramkar <devangmanjramkar@gmail.com>
2023-04-26 23:11:58 +0530

    Created PWA

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
-module.exports = {
+const withPWA = require("next-pwa");
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
   reactStrictMode: true,
-};
+});
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -10,7 +10,21 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* <meta name="viewport" content="width=device-width,minimum-scale=1, initial-scale=1" /> */}
+          <meta charset="utf-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+          />
+          <meta
+            name="description"
+            content="Next-Gen Blogger Powered By HyGraph"
+          />
+          <meta name="theme-color" content="#000" />
+          <title>Graphica Blogger</title>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" href="/apple-icon.png"></link>
         </Head>
         <body>
           <Main />
