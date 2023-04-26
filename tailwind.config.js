f41cb277209219f8fe24bfc55a00053895dac4b3
Devang Manjramkar <devangmanjramkar@gmail.com>
2023-04-26 22:49:53 +0530

    sda

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  // purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class", // or 'media' or 'class'
+  content: ["./components/**/*.{html,js,jsx}", "./pages/**/*.{html,js,jsx}"],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       spacing: {
